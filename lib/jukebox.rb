153c8e46d58a2737427541daaffe5017d10752e2
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-02 15:15:53 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,34 +26,34 @@ songs = [
 
 def help
   
-  puts: "I accept the following commands:"
-  puts: "- help : displays this help message"
-  puts: "- list : displays a list of songs you can play"
-  puts: "- play : lets you choose a song to play"
-  puts: "- exit : exits this program"
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
   
 end
+
+
 
 def list (songs)
   
   songs.each_with_index do |track, index|
-    
     puts "#{index}. #{track}"
-    
-  
   end
   
 end
 
 
-
-
-
-
-
-
-
-
+def play (songs)
+  
+  puts "Please enter a song name or number:"
+  user_selection = gets.chomp
+  
+  if songs.include?(user_selection) ? puts "Playing #{user_selection}" : puts "Invalid input, please try again"
+  
+  
+end
 
 
 
